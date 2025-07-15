dfa5724a87bf5e22c064356237ad1774946183c3
Game-FM <gamefm207@gmail.com>
2025-07-16 01:23:28 +0700

    Add files via upload

--- GameFM/scriptlogin.js
+++ GameFM/scriptlogin.js
@@ -10,8 +10,8 @@ function navigate(page) {
             </a>
             <div class="row justify-content-center" style="margin-top: 2vh; margin-bottom: 2vh;">
                 <div class="col-12 d-flex justify-content-center g-2" style="background-color: rgb(17, 14, 40); width: 90%; border-radius: 3vw; margin-bottom: 1vh;">
-                    <form action="">
-                        <div class="text-start mb-3 row mt-5 " style="padding-top: 1vw;">
+                    <form action="" class="m-4">
+                        <div class="text-start mb-5 row mt-5 " style="padding-top: 0.5vw;">
                             <div style="font-size: 1rem; color:  rgb(84, 48, 203);"><strong>ĐĂNG NHẬP BẰNG TÊN TÀI KHOẢN</strong></div>
                             <div><input class="login-input w-100" type="text" name="" id=""></div>
                         </div>
@@ -74,7 +74,7 @@ function navigate(page) {
             </a>
             <div class="row justify-content-center" style="margin-top: 2vh; margin-bottom: 2vh;">
                 <div class="col-12 d-flex justify-content-center g-2" style="background-color: rgb(17, 14, 40); width: 90%; border-radius: 3vw; margin-bottom: 1vh;">
-                    <form action="">
+                    <form action="" class="m-4">
                         <div class="row text-start mb-1 mt-4">
                             <div style="font-size: 1rem; color:  rgb(84, 48, 203);"><strong>USERNAME HOẶC TÊN CỦA BẠN</strong></div>
                             <div><input class="login-input w-100" type="text" name="" id=""></div>
